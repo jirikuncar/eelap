6a2551912234cb6be3f318a77fe695f417c44a37
Jiri Kuncar <jiri.kuncar@gmail.com>
2014-03-22 17:57:14 +0100

    Installation cleanup

---
+++ analysis.sh
@@ -0,0 +1,1 @@
+python generator.py --path 0 1 3 --reachability --with-communication-server -csP 0.012 -csQ 0.003 -t s 0.020 2 0.001 -t r 0.020 1 0.001 import < tests/data/system.xml
--- setup.py
+++ setup.py
@@ -1,6 +1,33 @@
-from setuptools import setup
-from setuptools import find_packages
+# -*- coding: utf-8 -*-
+##
+## This file is part of End-to-End Latency Analyzer for ProCom (EELAP).
+## Copyright (C) 2012, 2013, 2014 Jiri Kuncar <jiri.kuncar@gmail.com>.
+##
+## EELAP is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## EELAP is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+import os
+import re
+
+from setuptools import setup
+
+# Get the version string.  Cannot be done with import!
+with open(os.path.join('eelap', 'version.py'), 'rt') as f:
+    version = re.search(
+        '__version__\s*=\s*"(?P<version>.*)"\n',
+        f.read()
+    ).group('version')
 
 install_requires = [
     'setuptools',
@@ -30,19 +57,22 @@ with open("README.md") as f:
 with open("CHANGES.txt") as f:
     CHANGES = f.read()
 
-setup(name='eelap',
-      version='0.3.1',
-      packages=find_packages(),
-      description=("End-to-End Latency Analysis for ProCom"),
-      long_description=README + '\n' + CHANGES,
-      author='Jiri Kuncar',
-      author_email='jiri.kuncar@gmail.com',
-      include_package_data=True,
-      zip_safe=False,
-      classifiers=classifiers,
-      install_requires=install_requires,
-      keywords='latency analysis ProCom',
-      url='https://github.com/jirikuncar/eelap/',
-      license='gpl',
-      entry_points=entry_points,
-      )
+setup(
+    name='EELAP',
+    version=version,
+    license='GPLv2',
+    packages=['eelap'],
+    description=("End-to-End Latency Analysis for ProCom"),
+    long_description=README + '\n' + CHANGES,
+    author='Jiri Kuncar',
+    author_email='jiri.kuncar@gmail.com',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=classifiers,
+    install_requires=install_requires,
+    keywords='latency analysis ProCom',
+    url='https://github.com/jirikuncar/eelap/',
+    entry_points=entry_points,
+    test_suite='nose.collector',
+    tests_require=['nose', 'coverage', 'httpretty'],
+)
